8f814a4da93c75dff613c5cb086bce5f2fd12550
Om-Bhandwalkar02 <ombhandwalkar@outlook.com>
2025-03-01 04:20:30 +0530

    Update main

--- main.py
+++ main.py
@@ -16,6 +16,10 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
 app.include_router(post.router)
 app.include_router(user.router)
 app.include_router(auth.router)
